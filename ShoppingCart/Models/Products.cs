05b4918b5cf1d5ea7a3a9e8f2e068c85c6244b51
Arsh <arshhasan68@gmail.com>
2023-08-10 20:55:36 +0930

    Added products model

---
+++ ShoppingCart/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCart.Models
+{
+    public class Products : Controller
+    {
+        public IActionResult Index()
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            return View();
+        }
+    }
+}
